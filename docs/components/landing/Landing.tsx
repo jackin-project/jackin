4ae5fadeea186aafdaa18ee65c2c81a7abeb58fb
Alexey Zhokhov <alexey@zhokhov.com>
2026-04-20 00:05:36 +0000

    landing: add WordmarkFooter

--- docs/components/landing/Landing.tsx
+++ docs/components/landing/Landing.tsx
@@ -1,10 +1,12 @@
 // docs/components/landing/Landing.tsx
 import { InstallBlock } from './InstallBlock';
+import { WordmarkFooter } from './WordmarkFooter';
 
 export function Landing() {
   return (
     <div className="landing-root">
       <InstallBlock />
+      <WordmarkFooter />
     </div>
   );
 }
