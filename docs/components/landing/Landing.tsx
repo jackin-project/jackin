e9f3692e33cd34b137511b64ca4d287f8803a54b
Alexey Zhokhov <alexey@zhokhov.com>
2026-04-20 00:53:19 +0000

    landing: add HeroStage + HeroContent, assemble full page

--- docs/components/landing/Landing.tsx
+++ docs/components/landing/Landing.tsx
@@ -1,22 +1,24 @@
 // docs/components/landing/Landing.tsx
+import { HeroStage } from './HeroStage';
+import { VocabularyDictionary } from './VocabularyDictionary';
 import { PillCards } from './PillCards';
 import { ApproachCards } from './ApproachCards';
 import { CastRoster } from './CastRoster';
 import { CompositionMachine } from './CompositionMachine';
 import { DailyLoop } from './DailyLoop';
 import { InstallBlock } from './InstallBlock';
-import { VocabularyDictionary } from './VocabularyDictionary';
 import { WordmarkFooter } from './WordmarkFooter';
 
 export function Landing() {
   return (
     <div className="landing-root">
+      <HeroStage />
       <VocabularyDictionary />
       <PillCards />
       <ApproachCards />
       <CastRoster />
-      <DailyLoop />
       <CompositionMachine />
+      <DailyLoop />
       <InstallBlock />
       <WordmarkFooter />
     </div>
