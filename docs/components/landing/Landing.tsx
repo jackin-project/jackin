162d2d31d6fa3afab2edb2ce3c4905ee923eb7e4
Alexey Zhokhov <alexey@zhokhov.com>
2026-04-20 00:10:51 +0000

    landing: add PillCards (Section 03)

--- docs/components/landing/Landing.tsx
+++ docs/components/landing/Landing.tsx
@@ -1,10 +1,12 @@
 // docs/components/landing/Landing.tsx
+import { PillCards } from './PillCards';
 import { InstallBlock } from './InstallBlock';
 import { WordmarkFooter } from './WordmarkFooter';
 
 export function Landing() {
   return (
     <div className="landing-root">
+      <PillCards />
       <InstallBlock />
       <WordmarkFooter />
     </div>
