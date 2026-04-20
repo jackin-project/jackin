6dcd281410459800f52de99bdcc15687d20992a3
Alexey Zhokhov <alexey@zhokhov.com>
2026-04-20 00:33:56 +0000

    landing: add VocabularyDictionary (Section 02) scroll-driven

--- docs/components/landing/Landing.tsx
+++ docs/components/landing/Landing.tsx
@@ -4,11 +4,13 @@ import { ApproachCards } from './ApproachCards';
 import { CastRoster } from './CastRoster';
 import { CompositionMachine } from './CompositionMachine';
 import { InstallBlock } from './InstallBlock';
+import { VocabularyDictionary } from './VocabularyDictionary';
 import { WordmarkFooter } from './WordmarkFooter';
 
 export function Landing() {
   return (
     <div className="landing-root">
+      <VocabularyDictionary />
       <PillCards />
       <ApproachCards />
       <CastRoster />
